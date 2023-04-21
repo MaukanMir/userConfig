73b6c2691c8d60d0be99106b90dc909440ecf840
Maukan Mir <maukanm22@gmail.com>
2023-04-21 08:23:45 -0400

    Added new user information

--- src/main/java/com/userService/userService/service/UserService.java
+++ src/main/java/com/userService/userService/service/UserService.java
@@ -1,11 +1,18 @@
 package com.userService.userService.service;
 
+import com.userService.userService.domain.UserDomain;
 import com.userService.userService.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+    public void saveUser(UserDomain newUser){
+        userRepository.save(newUser);
+    }
 }
