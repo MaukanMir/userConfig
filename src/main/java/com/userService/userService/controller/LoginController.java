e44739b275e306fe708a49906e13ac5e3c671315
Maukan Mir <maukanm22@gmail.com>
2023-04-21 09:17:44 -0400

    Added new user service

--- src/main/java/com/userService/userService/controller/LoginController.java
+++ src/main/java/com/userService/userService/controller/LoginController.java
@@ -1,11 +1,20 @@
 package com.userService.userService.controller;
 
+import com.userService.userService.domain.UserDomain;
 import com.userService.userService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/login")
 @RestController
 public class LoginController {
     @Autowired
     UserService userService;
+
+    public ResponseEntity<UserDomain> authenticateUser(@RequestBody UserDomain loginForm){
+
+    }
 }
