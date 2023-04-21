6924147b31d102bf104db758ae2cd2090964aa47
Maukan Mir <maukanm22@gmail.com>
2023-04-21 08:52:43 -0400

    Saving a user is now done

--- src/main/java/com/userService/userService/controller/UserController.java
+++ src/main/java/com/userService/userService/controller/UserController.java
@@ -3,25 +3,19 @@ package com.userService.userService.controller;
 import com.userService.userService.domain.UserDomain;
 import com.userService.userService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/userService")
+import java.util.List;
+
+@RequestMapping("/userRegister")
 @RestController
 public class UserController {
     @Autowired
     UserService userService;
-//    @Autowired
-//    private BCrypt
 
-    @PostMapping()
-    public ResponseEntity<UserDomain> createUser(@RequestBody UserDomain user){
-        String hashedPassword;
-        userService.saveUser(user);
-        return ResponseEntity.ok(user);
+    @PostMapping
+    public ResponseEntity<List<UserDomain>> createUser(@RequestBody UserDomain user){
+         return ResponseEntity.ok(userService.saveUser(user));
     }
-
-
-
 }
