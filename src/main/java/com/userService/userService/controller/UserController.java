723b7beda763d103b4eb0118a1552eb73eca6973
Maukan Mir <maukanm22@gmail.com>
2023-04-21 10:50:23 -0400

    Added new user information

--- src/main/java/com/userService/userService/controller/UserController.java
+++ src/main/java/com/userService/userService/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.userService.userService.controller;
 
+import com.userService.userService.domain.UserDomain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/userBio")
+@RestController
 public class UserController {
+
+    @GetMapping("/findUser{Id}")
+    public UserDomain grabUserInfo(@PathVariable UserDomain Id){
+
+    }
 }
