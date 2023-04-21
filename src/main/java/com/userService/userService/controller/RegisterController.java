5d9d0ff0af014fc3ab7b19181028abe27eef824e
Maukan Mir <maukanm22@gmail.com>
2023-04-21 08:53:46 -0400

    Changed file name

--- src/main/java/com/userService/userService/controller/RegisterController.java
+++ src/main/java/com/userService/userService/controller/RegisterController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequestMapping("/userRegister")
 @RestController
-public class UserController {
+public class RegisterController {
     @Autowired
     UserService userService;
 
