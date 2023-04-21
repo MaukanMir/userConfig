da356edb0a4fc1000fc29ac470ac7786270b46f2
Maukan Mir <maukanm22@gmail.com>
2023-04-21 08:20:58 -0400

    Added new service

--- src/main/java/com/userService/userService/service/UserService.java
+++ src/main/java/com/userService/userService/service/UserService.java
@@ -1,7 +1,11 @@
 package com.userService.userService.service;
 
+import com.userService.userService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
 }
