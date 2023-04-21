a901d7936bf74c8e5ca3d4e1836af4463160745d
Maukan Mir <maukanm22@gmail.com>
2023-04-21 08:25:53 -0400

    Injected in new user service

--- src/main/java/com/userService/userService/controller/UserController.java
+++ src/main/java/com/userService/userService/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.userService.userService.controller;
 
 import com.userService.userService.domain.UserDomain;
+import com.userService.userService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
     @Autowired
-
+    UserService userService;
     @PostMapping("")
     public ResponseEntity<UserDomain> createUser(@RequestBody UserDomain user){
 
