849a4eaf388ba5aa3c5e372242404f45c7b20b3e
Maukan Mir <maukanm22@gmail.com>
2023-04-21 08:05:10 -0400

    Added new styles

--- src/main/java/com/userService/userService/repository/UserRepository.java
+++ src/main/java/com/userService/userService/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.userService.userService.repository;
 
-public class UserRepository {
+import com.userService.userService.domain.UserDomain;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@RepositoryRestResource(collectionResourceRel = "UserServiceRepository", path = "UserServiceRepository")
+public interface UserRepository extends MongoRepository<UserDomain, String> {
 }
