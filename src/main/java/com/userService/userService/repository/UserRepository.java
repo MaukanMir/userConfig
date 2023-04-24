9d616b5b8a02ee03f43dc4179e7e573b7e5de480
Maukan Mir <maukanm22@gmail.com>
2023-04-23 20:48:04 -0400

    Added new files

--- src/main/java/com/userService/userService/repository/UserRepository.java
+++ src/main/java/com/userService/userService/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.userService.userService.domain.UserDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "UserRepository", path = "UserRepository")
 public interface UserRepository extends MongoRepository<UserDomain, String> {
