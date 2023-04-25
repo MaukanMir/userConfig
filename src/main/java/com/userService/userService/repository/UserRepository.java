991cad1453b20517661d3203cfd06443c64ba011
Maukan Mir <maukanm22@gmail.com>
2023-04-24 20:42:03 -0400

    Added new changes

--- src/main/java/com/userService/userService/repository/UserRepository.java
+++ src/main/java/com/userService/userService/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.userService.userService.domain.UserDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 
 @RepositoryRestResource(collectionResourceRel = "userConfig", path = "userConfig")
 public interface UserRepository extends MongoRepository<UserDomain, String> {
-    UserDomain findByUserName(String username);
+  List<UserDomain> findByUserName(String username);
 }
