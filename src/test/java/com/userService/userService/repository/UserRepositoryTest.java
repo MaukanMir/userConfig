06e4c798b126f99327d073ac13ed6c2ae0f6bc59
Maukan Mir <maukanm22@gmail.com>
2023-04-25 12:20:58 -0400

    Adding new tests

--- src/test/java/com/userService/userService/repository/UserRepositoryTest.java
+++ src/test/java/com/userService/userService/repository/UserRepositoryTest.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserRepositoryTest {
 
     @Autowired
     UserRepository repository;
+
+    private static final LocalDate TODAY = LocalDate.now();
 
 }
