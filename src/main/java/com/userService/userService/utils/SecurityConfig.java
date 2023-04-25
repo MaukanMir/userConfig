d1c183d663110eb3713c7bee799759221ab51c5f
Maukan Mir <maukanm22@gmail.com>
2023-04-25 12:44:33 -0400

    Added new security class

--- src/main/java/com/userService/userService/utils/SecurityConfig.java
+++ src/main/java/com/userService/userService/utils/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.userService.userService.utils;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
