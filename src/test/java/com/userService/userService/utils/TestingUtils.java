7d393d467a36904ae272ae9e1f038ac5163c0d8f
Maukan Mir <maukanm22@gmail.com>
2023-04-25 12:49:23 -0400

    Added component class

--- src/test/java/com/userService/userService/utils/TestingUtils.java
+++ src/test/java/com/userService/userService/utils/TestingUtils.java
@@ -1,10 +1,12 @@
 package com.userService.userService.utils;
 
 import com.userService.userService.domain.UserDomain;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Component
 public class TestingUtils {
 
     public UserDomain createUserDomain(String username, String password, String email, LocalDate dob, LocalDateTime createdAt){
