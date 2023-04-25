902cf40be75c7dcadd5b550b5e0af3be76c3d1c5
Maukan Mir <maukanm22@gmail.com>
2023-04-24 20:58:14 -0400

    Added new user files

--- src/test/java/com/userService/userService/utils/TestingUtils.java
+++ src/test/java/com/userService/userService/utils/TestingUtils.java
@@ -1,4 +1,20 @@
 package com.userService.userService.utils;
 
+import com.userService.userService.domain.UserDomain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class TestingUtils {
+
+    public UserDomain createUserDomain(String username, String password, String email, LocalDate dob, LocalDateTime createdAt){
+
+        return UserDomain.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .dob(dob)
+                .createdAt(createdAt)
+                .build();
+    }
 }
