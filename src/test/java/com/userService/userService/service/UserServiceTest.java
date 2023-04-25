59070d344e8500dbad9c991536601b40a8b4e94d
Maukan Mir <maukanm22@gmail.com>
2023-04-25 12:51:25 -0400

    Added new tests

--- src/test/java/com/userService/userService/service/UserServiceTest.java
+++ src/test/java/com/userService/userService/service/UserServiceTest.java
@@ -1,7 +1,11 @@
 package com.userService.userService.service;
 
+import com.userService.userService.utils.TestingUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UserServiceTest {
+    @Autowired
+    TestingUtils testingUtils;
 }
