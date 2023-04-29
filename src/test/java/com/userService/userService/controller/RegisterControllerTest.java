809ee245566ca2b88d1017906179a475f4331c66
Maukan Mir <maukanm22@gmail.com>
2023-04-29 18:41:02 -0400

    Added new user information

--- src/test/java/com/userService/userService/controller/RegisterControllerTest.java
+++ src/test/java/com/userService/userService/controller/RegisterControllerTest.java
@@ -8,11 +8,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,10 +39,20 @@ public class RegisterControllerTest {
     }
 
     @Test
-    void userRegisterGoldenPaths(){
+    void userRegisterGoldenPaths() throws Exception{
         UserDomain newUser = testingUtils.createUserDomain("user1","1234","mm@gmail.com",TODAY,NOW);
 
+        this.mockMvc.perform(post("/userRegister")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newUser))
+
+                ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty());
+
         List<UserDomain> savedUser = this.userRepository.findByUsername(newUser.getUsername());
+
+        assertEquals(savedUser.get(0).getUsername(),newUser.getUsername());
 
 
 
