99ef372b70aac7cc04b9b7443c0e76759823afd0
Maukan Mir <maukanm22@gmail.com>
2023-04-29 13:05:54 -0400

    Added new styles

--- src/test/java/com/userService/userService/controller/LoginControllerTest.java
+++ src/test/java/com/userService/userService/controller/LoginControllerTest.java
@@ -1,20 +1,30 @@
 package com.userService.userService.controller;
 
+import com.userService.userService.domain.UserDomain;
 import com.userService.userService.repository.UserRepository;
+import com.userService.userService.utils.TestingUtils;
+import jdk.incubator.vector.VectorOperators;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LoginControllerTest {
+    private static final LocalDate TODAY = LocalDate.now();
+    private static final LocalDateTime NOW = LocalDateTime.now();
 
     @Autowired
     LoginController loginController;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    TestingUtils testingUtils;
 
     @AfterEach
     void cleanUp(){
@@ -23,7 +33,7 @@ public class LoginControllerTest {
 
     @Test
     void loginAuthenticatorGoldenPaths() throws Exception{
-
+        UserDomain loginForm = testingUtils.createUserDomain("user1","1234","mm@gmail.com",TODAY,NOW);
     }
 
     @Test
