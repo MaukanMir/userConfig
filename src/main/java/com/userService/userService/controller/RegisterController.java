54d1d83e430784773aba96689983846abc42fc87
Maukan Mir <maukanm22@gmail.com>
2023-04-29 19:58:01 -0400

    Added more settings to address cors issue

--- src/main/java/com/userService/userService/controller/RegisterController.java
+++ src/main/java/com/userService/userService/controller/RegisterController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/userRegister")
 @RestController
 public class RegisterController {
--- src/main/java/com/userService/userService/controller/UserController.java
+++ src/main/java/com/userService/userService/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.userService.userService.controller;
 
-import com.userService.userService.domain.UserDomain;
+
 import com.userService.userService.dto.UserDTO;
 import com.userService.userService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
-
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/userProfile")
 @RestController
 public class UserController {
