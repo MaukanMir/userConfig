49aafcbf00035d4d166b0365bb7a883c0bec55d6
Maukan Mir <maukanm22@gmail.com>
2023-04-29 13:01:51 -0400

    Adding new user styles

--- src/test/java/com/userService/userService/controller/LoginControllerTest.java
+++ src/test/java/com/userService/userService/controller/LoginControllerTest.java
@@ -1,9 +1,23 @@
 package com.userService.userService.controller;
 
+import com.userService.userService.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LoginControllerTest {
+
+    @Autowired
+    LoginController loginController;
+    @Autowired
+    UserRepository userRepository;
+
+    @AfterEach
+    void cleanUp(){
+        this.userRepository.deleteAll();
+    }
+
 }
