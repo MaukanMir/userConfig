d46f9ae1663921afd877de5901092394ff59b579
Maukan Mir <maukanm22@gmail.com>
2023-04-29 18:31:35 -0400

    Added new user information

--- src/test/java/com/userService/userService/controller/RegisterControllerTest.java
+++ src/test/java/com/userService/userService/controller/RegisterControllerTest.java
@@ -3,7 +3,9 @@ package com.userService.userService.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.userService.userService.repository.UserRepository;
 import com.userService.userService.service.UserService;
+import com.userService.userService.utils.TestingUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,9 +19,17 @@ public class RegisterControllerTest {
     UserRepository userRepository;
     @Autowired
     RegisterController registerController;
+    @Autowired
+    TestingUtils testingUtils;
     @AfterEach
     void cleanUp(){
         this.userRepository.deleteAll();
     }
+
+    @Test
+    void userRegisterGoldenPaths(){
+
+    }
+
 
 }
