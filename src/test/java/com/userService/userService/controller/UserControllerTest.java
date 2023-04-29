7282bf04477feca23625e36227e24270c8ba0006
Maukan Mir <maukanm22@gmail.com>
2023-04-29 19:03:15 -0400

    Fixing repository

--- src/test/java/com/userService/userService/controller/UserControllerTest.java
+++ src/test/java/com/userService/userService/controller/UserControllerTest.java
@@ -1,7 +1,9 @@
 package com.userService.userService.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.userService.userService.repository.UserRepository;
 import com.userService.userService.utils.TestingUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,4 +18,11 @@ public class UserControllerTest {
     MockMvc mockMvc;
     @Autowired
     TestingUtils testingUtils;
+    @Autowired
+    UserRepository userRepository;
+
+    @AfterEach
+    void cleanUp(){
+        this.userRepository.deleteAll();
+    }
 }
