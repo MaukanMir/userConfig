1a6c8a9a2a7be37e13093bddf68d96d854161ac0
Maukan Mir <maukanm22@gmail.com>
2023-04-29 19:21:46 -0400

    Added DTO Wrapper function name change so its more readble for future me

--- src/main/java/com/userService/userService/service/UserService.java
+++ src/main/java/com/userService/userService/service/UserService.java
@@ -39,7 +39,7 @@ public class UserService {
         return  userRepository.findById(Id);
     }
 
-    public UserDTO transformObject(List UserDomain){
+    public UserDTO UserDTOWrapper(List UserDomain){
 
     }
 
