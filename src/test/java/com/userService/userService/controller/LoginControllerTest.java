ee3c00f825f38fc290b72d34d726d7047c85fa78
Maukan Mir <maukanm22@gmail.com>
2023-04-29 13:21:11 -0400

    Working on adding mvc work

--- src/test/java/com/userService/userService/controller/LoginControllerTest.java
+++ src/test/java/com/userService/userService/controller/LoginControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 
@@ -31,6 +32,8 @@ public class LoginControllerTest {
     UserRepository userRepository;
     @Autowired
     TestingUtils testingUtils;
+    @Autowired
+    private MockMvc mockMvc;
     private ObjectMapper objectMapper;
 
     @AfterEach
