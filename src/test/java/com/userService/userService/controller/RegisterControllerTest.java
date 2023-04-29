c121ecea5e6c38e56591c6a46930685577520efc
Maukan Mir <maukanm22@gmail.com>
2023-04-29 13:43:51 -0400

    Added new register tests

--- src/test/java/com/userService/userService/controller/RegisterControllerTest.java
+++ src/test/java/com/userService/userService/controller/RegisterControllerTest.java
@@ -1,4 +1,13 @@
 package com.userService.userService.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class RegisterControllerTest {
+    @Autowired
+    ObjectMapper objectMapper;
 }
