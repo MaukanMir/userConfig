8b6824c1b6fbb0e39cd84177136b130950cbdc56
Maukan Mir <maukanm22@gmail.com>
2023-04-29 18:59:05 -0400

    Added new updated

--- src/test/java/com/userService/userService/controller/UserControllerTest.java
+++ src/test/java/com/userService/userService/controller/UserControllerTest.java
@@ -1,4 +1,19 @@
 package com.userService.userService.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.userService.userService.utils.TestingUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    TestingUtils testingUtils;
 }
