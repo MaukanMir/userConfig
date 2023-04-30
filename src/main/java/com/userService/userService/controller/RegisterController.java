146fadafcc934178de152a037f5eb08408dcb47a
Maukan Mir <maukanm22@gmail.com>
2023-04-30 15:08:24 -0400

    Fixed controller with new error handling

--- src/main/java/com/userService/userService/controller/RegisterController.java
+++ src/main/java/com/userService/userService/controller/RegisterController.java
@@ -1,9 +1,12 @@
 package com.userService.userService.controller;
 
+import com.userService.userService.controller.exception.DuplicateEmailException;
+import com.userService.userService.controller.exception.DuplicateUsernameException;
 import com.userService.userService.domain.UserDomain;
 import com.userService.userService.dto.UserDTO;
 import com.userService.userService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +18,13 @@ public class RegisterController {
     UserService userService;
 
     @PostMapping
-    public UserDTO createUser(@RequestBody UserDomain user){
-         return (userService.saveUser(user));
+    public ResponseEntity<?>createUser(@RequestBody UserDomain user){
+        try{
+            return ResponseEntity.ok(userService.saveUser(user));
+        }catch( DuplicateUsernameException exception){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+        }catch(DuplicateEmailException exception){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+        }
     }
 }
