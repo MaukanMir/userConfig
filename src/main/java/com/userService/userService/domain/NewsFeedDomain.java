f6bc142ff56f40e9a6f47cfb61f13d7a353a5593
Maukan Mir <maukanm22@gmail.com>
2023-05-06 19:54:38 -0400

    Added new user styles

--- src/main/java/com/userService/userService/domain/NewsFeedDomain.java
+++ src/main/java/com/userService/userService/domain/NewsFeedDomain.java
@@ -1,4 +1,20 @@
 package com.userService.userService.domain;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Document("news_feed")
+@Data
+@Builder
 public class NewsFeedDomain {
+    @Id
+    String Id;
+    String email;
+    String username;
+    Map<String,String> metaData;
+
 }
