570b9c46517a9db4089c8d044441918423259563
Maukan Mir <maukanm22@gmail.com>
2023-05-06 20:30:56 -0400

    Working on adding user news feed information

--- src/main/java/com/userService/userService/service/NewsFeedService.java
+++ src/main/java/com/userService/userService/service/NewsFeedService.java
@@ -1,7 +1,18 @@
 package com.userService.userService.service;
 
+import com.userService.userService.domain.NewsFeedDomain;
+import com.userService.userService.repository.NewsFeedRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NewsFeedService {
+    @Autowired
+    NewsFeedRepository newsFeedRepository;
+
+    public List<NewsFeedDomain> saveUserNewsFeed(NewsFeedDomain userFeed){
+
+    }
 }
