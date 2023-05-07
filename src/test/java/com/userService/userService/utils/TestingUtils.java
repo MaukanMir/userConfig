9c005805dbd649f8ef2ce994815f30d3ce1e9841
Maukan Mir <maukanm22@gmail.com>
2023-05-06 20:33:57 -0400

    Added newsfeed class builder

--- src/test/java/com/userService/userService/utils/TestingUtils.java
+++ src/test/java/com/userService/userService/utils/TestingUtils.java
@@ -1,15 +1,17 @@
 package com.userService.userService.utils;
 
+import com.userService.userService.domain.NewsFeedDomain;
 import com.userService.userService.domain.UserDomain;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Component
 public class TestingUtils {
 
-    public UserDomain createUserDomain(String username, String password, String email, LocalDate dob, LocalDateTime createdAt){
+    public UserDomain createUserDomain(String username, String password, String email, LocalDate dob, LocalDateTime createdAt) {
 
         return UserDomain.builder()
                 .username(username)
@@ -17,6 +19,14 @@ public class TestingUtils {
                 .email(email)
                 .dob(dob)
                 .createdAt(createdAt)
+                .build();
+    }
+
+    public NewsFeedDomain createNewsFeed(String email, String username, Map<String, String> metaData) {
+        return NewsFeedDomain.builder()
+                .email(email)
+                .username(username)
+                .metaData(metaData)
                 .build();
     }
 }
