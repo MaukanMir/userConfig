8cd6e4231a607b57c1852670ce2a47ee20c81218
Maukan Mir <maukanm22@gmail.com>
2023-05-06 20:21:02 -0400

    Added new user information

--- src/main/java/com/userService/userService/repository/NewsFeedRepository.java
+++ src/main/java/com/userService/userService/repository/NewsFeedRepository.java
@@ -1,7 +1,10 @@
 package com.userService.userService.repository;
 
+import com.userService.userService.domain.NewsFeedDomain;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "newsFeed", path = "newsFeed")
-public class NewsFeedRepository {
+public interface NewsFeedRepository extends MongoRepository<NewsFeedDomain, String> {
+
 }
