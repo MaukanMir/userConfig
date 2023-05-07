0c19a2bbef44ecd3dd240dc0d24cbba09e7460f0
Maukan Mir <maukanm22@gmail.com>
2023-05-06 20:19:25 -0400

    Added new newsfeed controller

--- src/main/java/com/userService/userService/repository/NewsFeedRepository.java
+++ src/main/java/com/userService/userService/repository/NewsFeedRepository.java
@@ -1,4 +1,7 @@
 package com.userService.userService.repository;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "newsFeed", path = "newsFeed")
 public class NewsFeedRepository {
 }
