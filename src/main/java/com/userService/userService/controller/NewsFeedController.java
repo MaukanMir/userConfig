3b1500361a897a9260c02c46a63a25ca4d84ddc0
Maukan Mir <maukanm22@gmail.com>
2023-05-06 20:42:53 -0400

    Return users news feed

--- src/main/java/com/userService/userService/controller/NewsFeedController.java
+++ src/main/java/com/userService/userService/controller/NewsFeedController.java
@@ -1,8 +1,23 @@
 package com.userService.userService.controller;
 
+import com.userService.userService.domain.NewsFeedDomain;
+import com.userService.userService.service.NewsFeedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequestMapping("/newsFeed")
 @RestController
 public class NewsFeedController {
+    @Autowired
+    NewsFeedService newsFeedService;
+    @GetMapping("search/findByUsername/")
+    public List<NewsFeedDomain> getUserNewsFeed(@RequestBody NewsFeedDomain username){
+
+    }
 
 }
