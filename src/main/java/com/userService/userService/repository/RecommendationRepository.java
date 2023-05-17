d51419a99c943d711b1725948a3dffe1e65a5a54
Maukan Mir <maukanm22@gmail.com>
2023-05-16 20:12:50 -0400

    Added recommendation feed

--- src/main/java/com/userService/userService/repository/RecommendationRepository.java
+++ src/main/java/com/userService/userService/repository/RecommendationRepository.java
@@ -1,4 +1,12 @@
 package com.userService.userService.repository;
 
-public class RecommendationRepository {
+
+import com.userService.userService.domain.RecommendationFeedDomain;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "recommendationFeed", path = "recommendationFeed")
+public interface RecommendationRepository extends MongoRepository<RecommendationFeedDomain,String> {
+
 }
+
