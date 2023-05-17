ce507795e7deb079805d15d3983da39ff5540e58
Maukan Mir <maukanm22@gmail.com>
2023-05-16 20:40:54 -0400

    Added new service method

--- src/main/java/com/userService/userService/repository/RecommendationRepository.java
+++ src/main/java/com/userService/userService/repository/RecommendationRepository.java
@@ -5,8 +5,10 @@ import com.userService.userService.domain.RecommendationFeedDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "recommendationFeed", path = "recommendationFeed")
 public interface RecommendationRepository extends MongoRepository<RecommendationFeedDomain,String> {
-
+    List<RecommendationFeedDomain> findByUsername(String username);
 }
 
--- src/main/java/com/userService/userService/service/RecommendationFeedService.java
+++ src/main/java/com/userService/userService/service/RecommendationFeedService.java
@@ -1,11 +1,18 @@
 package com.userService.userService.service;
 
+import com.userService.userService.domain.RecommendationFeedDomain;
 import com.userService.userService.repository.RecommendationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RecommendationFeedService {
     @Autowired
     RecommendationRepository recommendationRepository;
+
+    public List<RecommendationFeedDomain> sendUserFeed(String username){
+        return recommendationRepository.findByUsername(username);
+    }
 }
