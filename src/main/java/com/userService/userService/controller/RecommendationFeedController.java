a00fdb9bbb763c462daf9b9c2cf6fbaa3f7dbbc9
Maukan Mir <maukanm22@gmail.com>
2023-05-16 20:56:30 -0400

    Added new controller page

--- src/main/java/com/userService/userService/controller/RecommendationFeedController.java
+++ src/main/java/com/userService/userService/controller/RecommendationFeedController.java
@@ -1,4 +1,8 @@
 package com.userService.userService.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RecommendationFeedController {
 }
