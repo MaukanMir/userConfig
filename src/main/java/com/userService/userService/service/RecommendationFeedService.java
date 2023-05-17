60265855f2a5484193f2bbefa30f13d22e5c15d3
Maukan Mir <maukanm22@gmail.com>
2023-05-16 20:38:25 -0400

    Added new service

--- src/main/java/com/userService/userService/service/RecommendationFeedService.java
+++ src/main/java/com/userService/userService/service/RecommendationFeedService.java
@@ -1,4 +1,11 @@
 package com.userService.userService.service;
 
+import com.userService.userService.repository.RecommendationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecommendationFeedService {
+    @Autowired
+    RecommendationRepository recommendationRepository;
 }
