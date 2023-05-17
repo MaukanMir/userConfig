93a30a44f83846304e80f70ab0bb504bea70603e
Maukan Mir <maukanm22@gmail.com>
2023-05-16 20:10:45 -0400

    Added changes to the metaData

--- src/main/java/com/userService/userService/domain/RecommendationFeedDomain.java
+++ src/main/java/com/userService/userService/domain/RecommendationFeedDomain.java
@@ -2,10 +2,18 @@ package com.userService.userService.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
 
 @Document("recommendation_feed")
 @Builder
 @Data
 public class RecommendationFeedDomain {
+    @Id
+    private String Id;
+    private String username;
+    private String email;
+    private Map<String,Object> metaData;
 }
