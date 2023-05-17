d9046556bdbce02a55b8e42c3c2dd425e03f7c12
Maukan Mir <maukanm22@gmail.com>
2023-05-16 21:00:08 -0400

    Added new styling

--- src/main/java/com/userService/userService/controller/RecommendationFeedController.java
+++ src/main/java/com/userService/userService/controller/RecommendationFeedController.java
@@ -1,8 +1,23 @@
 package com.userService.userService.controller;
 
-import org.springframework.stereotype.Controller;
+import com.userService.userService.domain.RecommendationFeedDomain;
+import com.userService.userService.service.RecommendationFeedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/recommendationService")
 public class RecommendationFeedController {
+    @Autowired
+    RecommendationFeedService recommendationFeedService;
+
+    @GetMapping("/")
+    public List<RecommendationFeedDomain> findUserRecommendation(@RequestParam String username){
+        return recommendationFeedService.sendUserFeed(username);
+    }
 }
