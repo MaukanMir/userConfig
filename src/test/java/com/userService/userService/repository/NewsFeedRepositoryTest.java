09a1cb5ae62aeb735657b53d4b4986e58c3ffd6d
Maukan Mir <maukanm22@gmail.com>
2023-05-18 12:46:41 -0400

    Working on fixing repository

--- src/test/java/com/userService/userService/repository/NewsFeedRepositoryTest.java
+++ src/test/java/com/userService/userService/repository/NewsFeedRepositoryTest.java
@@ -1,7 +1,16 @@
 package com.userService.userService.repository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class NewsFeedRepositoryTest {
+    @Autowired
+    NewsFeedRepository newsFeedRepository;
+
+    @AfterEach
+    void  cleanUp(){
+        newsFeedRepository.deleteAll();
+    }
 }
